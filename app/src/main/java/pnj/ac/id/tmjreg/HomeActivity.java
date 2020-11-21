f86dbda04815bd5ceb40001607afd62d4fa221fb
Brandhana FS <brandhana.fajri@gmail.com>
2020-11-21 22:50:21 +0700

    TUGAS LATIHAN LOGIN - Brandhana FS (1807421010)

--- app/src/main/java/pnj/ac/id/tmjreg/HomeActivity.java
+++ app/src/main/java/pnj/ac/id/tmjreg/HomeActivity.java
@@ -3,9 +3,11 @@ package pnj.ac.id.tmjreg;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,43 +15,54 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class HomeActivity extends AppCompatActivity {
 
-    SharedPreferences preferences;
-    TextView txtEmail, txtNama;
+    //SharedPreferences preferences;
+    TextView txtEmail, txtNama, txtJamLahir, txtTglLahir;
+    public String[] doc;
+    public static String FILENAME = "register.txt";
 
-    Button actionInternalStorage,actionEksternalStorage;
+    //Button actionInternalStorage,actionEksternalStorage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        preferences = getSharedPreferences("profile", MODE_PRIVATE);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtNama = findViewById(R.id.txtNama);
+        //preferences = getSharedPreferences("profile", MODE_PRIVATE);
+        txtEmail = findViewById(R.id.Email);
+        txtNama = findViewById(R.id.Nama);
+        txtJamLahir = findViewById(R.id.JamLahir);
+        txtTglLahir = findViewById(R.id.TglLahir);
+        bacaData();
 
-        actionInternalStorage = findViewById(R.id.actionInternal);
-        actionEksternalStorage = findViewById(R.id.actionEksternalStorage);
+//        actionInternalStorage = findViewById(R.id.actionInternal);
+//        actionEksternalStorage = findViewById(R.id.actionEksternalStorage);
 
-        txtEmail.setText(preferences.getString("email", ""));
-        txtNama.setText(preferences.getString("nama", ""));
+        txtEmail.setText(doc[0]);
+        txtNama.setText(doc[2]);
+        txtJamLahir.setText(doc[3]);
+        txtTglLahir.setText(doc[4]);
 
-        actionInternalStorage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, InternalStorage.class);
-                startActivity(intent);
-            }
-        });
-
-        actionEksternalStorage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, EksternalStorage.class);
-                startActivity(intent);
-            }
-        });
+//        actionInternalStorage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(HomeActivity.this, InternalStorage.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        actionEksternalStorage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(HomeActivity.this, EksternalStorage.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 
@@ -63,13 +76,42 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId()==R.id.menu_tambah) {
-            Intent tambah = new Intent(HomeActivity.this, InputDataMahasiswa.class);
-            startActivity(tambah);
-        }else {
+        switch (item.getItemId()){
+            case R.id.Logout:
+                SharedPreferences sharedPreferences = this.getSharedPreferences("isLogin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sharedPreferences.edit();
+                edit.clear();
+                edit.commit();
 
+                Intent intent3 = new Intent(this, LoginActivity.class);
+                startActivity(intent3);
+                this.finish();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    void bacaData(){
+        File file = new File(getFilesDir(),FILENAME);
+
+        if(file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line = bufferedReader.readLine();
+
+                while (line !=null){
+                    text.append(line);
+                    line = bufferedReader.readLine();
+                }
+
+                bufferedReader.close();
+            }catch (Exception e){
+                Log.e("ERROR",""+e.getMessage());
+            }
+            doc = text.toString().split(" ");
+        }
     }
 }
--- app/src/main/java/pnj/ac/id/tmjreg/LoginActivity.java
+++ app/src/main/java/pnj/ac/id/tmjreg/LoginActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,36 +23,38 @@ public class LoginActivity extends AppCompatActivity {
     TextView txtRegister;
 
     SharedPreferences preferences;
+    public String[] doc;
+    public static String FILENAME = "register.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        preferences = getSharedPreferences("profile", MODE_PRIVATE);
+        preferences = getSharedPreferences("LoginStatus", MODE_PRIVATE);
 
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         actionLogin = findViewById(R.id.actionLogin);
         txtRegister = findViewById(R.id.txtRegister);
+        File file = new File(getFilesDir(),FILENAME);
+        bacaData();
 
         actionLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(edtEmail.getText().toString().length() > 0 && edtPassword.getText().toString().length()>0) {
                     //cek login
-                    if(edtEmail.getText().toString().equals("syamsi@mail.com") && edtPassword.getText().toString().equals("1234")) {
+                    if(edtEmail.getText().toString().equals(doc[0]) && edtPassword.getText().toString().equals(doc[1])) {
                         //sukses login
-
-                        SharedPreferences.Editor edit = preferences.edit();
-
-                        edit.putString("email", "syamsi@mail.com");
-                        edit.putString("nama", "syamsi dwi cahya");
-                        edit.putBoolean("isLogin", true);
-                        edit.commit();
-
                         Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                         startActivity(intent);
+                        SharedPreferences.Editor edit = preferences.edit();
+
+//                        edit.putString("email", "syamsi@mail.com");
+//                        edit.putString("nama", "syamsi dwi cahya");
+                        edit.putBoolean("isLogin", true);
+                        edit.commit();
                         finish();
                     }else {
                         Toast.makeText(LoginActivity.this, "Mohon Maaf Email dan Password Salah", Toast.LENGTH_SHORT).show();
@@ -66,5 +73,28 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    void bacaData(){
+        File file = new File(getFilesDir(),FILENAME);
+
+        if(file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line = bufferedReader.readLine();
+
+                while (line !=null){
+                    text.append(line);
+                    line = bufferedReader.readLine();
+                }
+
+                bufferedReader.close();
+            }catch (Exception e){
+                Log.e("ERROR",""+e.getMessage());
+            }
+            doc = text.toString().split(" ");
+        }
     }
 }
