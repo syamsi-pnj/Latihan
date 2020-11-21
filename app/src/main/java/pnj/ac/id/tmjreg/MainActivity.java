87e69f77b14ef7592f1db0c6a4965f7e0f29259e
BAY PC <bayupras428@gmail.com>
2020-11-21 21:28:57 +0700

    Latihan 2 Bayu

--- app/src/main/java/pnj/ac/id/tmjreg/MainActivity.java
+++ app/src/main/java/pnj/ac/id/tmjreg/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        preferences = getSharedPreferences("profile", MODE_PRIVATE);
+        preferences = getSharedPreferences("isLogin", MODE_PRIVATE);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
